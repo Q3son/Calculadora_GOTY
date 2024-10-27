8675ad4db643e36a440ac29803b3fe1963675c2f
Q3son <esubiahu@unsa.edu.pe>
2024-10-26 23:38:46 -0500

    VERSIÓN DEFINITIVA Y FINAL DE LA CALCULADORA:
    - Se ha decidido finalmente eliminar la función EVAL de perl, porque este simplificaba mucho la dificultad que debía tener la creación de la calculadora, por consecuencia el perl ahora es el protagonista

--- cgi-bin/calculadora.pl
+++ cgi-bin/calculadora.pl
@@ -1,41 +1,153 @@
 #!/usr/bin/perl
-use utf8;                              # Permite el uso de caracteres UTF-8
-use strict;                           # Habilita restricciones de declaración
-use warnings;                         # Activa advertencias sobre posibles errores
-use CGI qw(:standard);                # Importa la biblioteca CGI
-use URI::Escape;                      # Permite la codificación de URI
-use List::Util qw(min max);          # Importa min y max desde List::Util
 
-# Crear un nuevo objeto CGI
-my $cgi = CGI->new;
+use strict;
+use warnings;
+use CGI qw(:standard);
+use List::Util qw(min max);
 
-# Imprimir cabeceras HTTP
-print $cgi->header('text/html;charset=UTF-8');
+print header('text/html; charset=UTF-8');
+print start_html("Calculadora teamQ3son");
 
-# Capturar la expresión enviada desde el formulario
-my $expresion = $cgi->param('expresion');
+my $expresion = param('expresion') || '';
+my $resultado = '';
 
-# Inicializar la variable resultado
-my $resultado;
-
-# Validar la expresión usando expresiones regulares
-if ($expresion =~ /^[\d\s+\-*\/\^().]+$/) {
-
-    # Evaluar la expresión
-    eval {
-        $resultado = eval($expresion);
-        1;  # Retornar verdadero si no hay errores
-    } or do {
-        print $cgi->p("Error en la evaluación de la expresión: $@");
-        exit;
-    };
-} else {
-    print $cgi->p("Expresión inválida. Por favor, utiliza números y operadores válidos.");
-}
-# Imprimir el resultado
-if (defined $resultado) {
-    print $cgi->p("Resultado: $resultado");
+if ($expresion) {
+    if ($expresion =~ /^[\d\s+\-*\/\^().]+$/) {
+        $resultado = evaluar_expresion($expresion);
+    } else {
+        $resultado = "Error: Caracteres no válidos en la expresión.";
+    }
 }
 
-# Finalizar el script
-print $cgi->end_html();
+print <<HTML;
+    <h1>Calculadora teamQ3son</h1>
+    <form action="calculadora.pl" method="get">
+        <div>
+            <input type="text" id="pantalla" name="expresion" placeholder="Ingresa tu operación aquí" value="$expresion" readonly>
+        </div>
+        <div id="botones">
+            <button type="button" onclick="agregarValor('7')">7</button>
+            <button type="button" onclick="agregarValor('8')">8</button>
+            <button type="button" onclick="agregarValor('9')">9</button>
+            <button type="button" onclick="agregarValor('+')">+</button>
+            <button type="button" onclick="agregarValor('4')">4</button>
+            <button type="button" onclick="agregarValor('5')">5</button>
+            <button type="button" onclick="agregarValor('6')">6</button>
+            <button type="button" onclick="agregarValor('-')">-</button>
+            <button type="button" onclick="agregarValor('1')">1</button>
+            <button type="button" onclick="agregarValor('2')">2</button>
+            <button type="button" onclick="agregarValor('3')">3</button>
+            <button type="button" onclick="agregarValor('*')">*</button>
+            <button type="button" onclick="agregarValor('0')">0</button>
+            <button type="button" onclick="agregarValor('/')">/</button>
+            <button type="button" onclick="agregarValor('^')">^</button>
+            <button type="button" onclick="agregarValor('(')">(</button>
+            <button type="button" onclick="agregarValor(')')">)</button>
+            <button type="submit">Calcular</button>
+            <button type="button" onclick="limpiar()">Limpiar</button>
+            <button type="button" onclick="retroceder()">←</button>
+        </div>
+        <div id="mensajeError" style="color: red; margin-top: 10px;">Resultado: $resultado</div>
+    </form>
+
+    <script>
+        function agregarValor(valor) {
+            const pantalla = document.getElementById('pantalla');
+            pantalla.value += valor;
+        }
+
+        function limpiar() {
+            document.getElementById('pantalla').value = '';
+        }
+
+        function retroceder() {
+            const pantalla = document.getElementById('pantalla');
+            pantalla.value = pantalla.value.slice(0, -1);
+        }
+    </script>
+HTML
+
+print end_html;
+
+sub evaluar_expresion {
+    my ($expresion) = @_;
+
+    # Separar la expresión en tokens (números y operadores)
+    my @tokens = split(/(\D)/, $expresion);
+    my @resultado;
+
+    foreach my $token (@tokens) {
+        if ($token =~ /^\s*$/) {
+            next;  # Ignorar espacios
+        }
+        push @resultado, $token;  # Agregar tokens válidos
+    }
+
+    return calcular(\@resultado);
+}
+
+sub calcular {
+    my ($tokens) = @_;
+
+    # Manejo de operaciones en paréntesis
+    while (my ($i) = grep { $tokens->[$_] eq '(' } (0..$#$tokens)) {
+        my $inicio = $i;
+        my $fin = $inicio + 1;
+
+        # Encontrar el cierre del paréntesis
+        my $nivel = 1;
+        while ($nivel > 0 && $fin <= $#$tokens) {
+            if ($tokens->[$fin] eq '(') {
+                $nivel++;
+            } elsif ($tokens->[$fin] eq ')') {
+                $nivel--;
+            }
+            $fin++;
+        }
+
+        # Reemplazar la expresión entre paréntesis por su resultado
+        my $subexp = [@$tokens[$inicio + 1 .. $fin - 2]];  # La subexpresión dentro de paréntesis
+        my $subresultado = calcular($subexp);
+        splice @$tokens, $inicio, $fin - $inicio, $subresultado;  # Reemplazar en la lista de tokens
+    }
+
+    # Manejo de operaciones de potencia
+    while (my ($i) = grep { $tokens->[$_] eq '^' } (0..$#$tokens)) {
+        my $base = $tokens->[$i - 1];
+        my $exponente = $tokens->[$i + 1];
+        my $resultado = $base ** $exponente;
+        splice @$tokens, $i - 1, 3, $resultado;  # Reemplazar en la lista de tokens
+    }
+
+    # Manejo de multiplicación y división
+    while (my ($i) = grep { $tokens->[$_] eq '*' || $tokens->[$_] eq '/' } (0..$#$tokens)) {
+        my $izquierda = $tokens->[$i - 1];
+        my $derecha = $tokens->[$i + 1];
+
+        my $resultado;
+        if ($tokens->[$i] eq '*') {
+            $resultado = $izquierda * $derecha;
+        } elsif ($tokens->[$i] eq '/') {
+            $resultado = $derecha != 0 ? $izquierda / $derecha : 'Error: División por cero';
+        }
+
+        splice @$tokens, $i - 1, 3, $resultado;  # Reemplazar en la lista de tokens
+    }
+
+    # Manejo de suma y resta
+    while (my ($i) = grep { $tokens->[$_] eq '+' || $tokens->[$_] eq '-' } (0..$#$tokens)) {
+        my $izquierda = $tokens->[$i - 1];
+        my $derecha = $tokens->[$i + 1];
+
+        my $resultado;
+        if ($tokens->[$i] eq '+') {
+            $resultado = $izquierda + $derecha;
+        } elsif ($tokens->[$i] eq '-') {
+            $resultado = $izquierda - $derecha;
+        }
+
+        splice @$tokens, $i - 1, 3, $resultado;  # Reemplazar en la lista de tokens
+    }
+
+    return $tokens->[0];  # El resultado final debe ser el único valor en los tokens
+}
