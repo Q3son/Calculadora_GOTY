443dcf29e5b7408a210e813c13655e7464211475
Q3son <esubiahu@unsa.edu.pe>
2024-10-26 19:30:14 -0500

    Segunda versión del scropt de Perl:
    -Hacemos la captura de la expresión desde el formulario y la validación con expresiones regulares.

--- cgi-bin/calculadora.pl
+++ cgi-bin/calculadora.pl
@@ -14,3 +14,21 @@ print $cgi->header('text/html;charset=UTF-8');
 
 # Capturar la expresión enviada desde el formulario
 my $expresion = $cgi->param('expresion');
+
+# Inicializar la variable resultado
+my $resultado;
+
+# Validar la expresión usando expresiones regulares
+if ($expresion =~ /^[\d\s+\-*\/\^().]+$/) {
+    
+    # Evaluar la expresión
+    eval {
+        $resultado = eval($expresion);
+        1;  # Retornar verdadero si no hay errores
+    } or do {
+        print $cgi->p("Error en la evaluación de la expresión: $@");
+        exit;
+    };
+} else {
+    print $cgi->p("Expresión inválida. Por favor, utiliza números y operadores válidos.");
+}
